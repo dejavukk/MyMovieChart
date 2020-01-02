c77c15ad48e848f0426c4fb2831697f96848581d
junhyuk <dejavukk1@gmail.com>
2020-01-02 23:47:07 +0900

    튜플 아이템으로 데이터 세트 구성.

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -10,39 +10,32 @@ import UIKit
 
 class ListViewContoller: UITableViewController {
     
+    // 튜플 아이템을 가진 배열로 정의된 데이터 세트
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비.", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "대만 청춘 로맨스, 너까지 다섯 걸음.", "2015-05-07", 9.1)
+    ]
+    
     // 테이블 뷰를 구성할 리스트 데이터
-    var list = [MovieVO]()
+    
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        return datalist
+        
+    }()
     
     override func viewDidLoad() {
-        // 첫번째 행
-        var mvo = MovieVO()
-        mvo.title = "다크나이트"
-        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
-        mvo.opendate = "2008-09-04"
-        mvo.rating = 8.95
-        
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        //두번째 행
-        mvo = MovieVO()
-        mvo.title = "호우시절"
-        mvo.description = "때를 알고 내리는 좋은 비"
-        mvo.opendate = "2009-10-08"
-        mvo.rating = 7.31
-        
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        //세번째 행
-        mvo = MovieVO()
-        mvo.title = "말할 수 없는 비밀"
-        mvo.description = "대만 청춘 로맨스"
-        mvo.opendate = "2015-05-07"
-        mvo.rating = 9.19
-        
-        // 배열에 추가
-        self.list.append(mvo)
         
     }
     
