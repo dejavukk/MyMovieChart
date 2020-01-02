2b9665866c81817d2cfd31a39d9ec50cea440284
junhyuk <dejavukk1@gmail.com>
2020-01-03 00:54:51 +0900

    테이블뷰셀에 데이터 표시 성공.

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -14,7 +14,7 @@ class ListViewContoller: UITableViewController {
     var dataset = [
         ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
         ("호우시절", "때를 알고 내리는 좋은 비.", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "대만 청춘 로맨스, 너까지 다섯 걸음.", "2015-05-07", 9.1)
+        ("말할 수 없는 비밀", "대만 청춘 로맨스, 너까지 다섯 걸음.", "2015-05-07", 9.19)
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
@@ -31,28 +31,33 @@ class ListViewContoller: UITableViewController {
             datalist.append(mvo)
         }
         return datalist
-        
     }()
     
     override func viewDidLoad() {
         
     }
     
+    // 테이블 뷰 행의 갯수를 반환하는 메소드.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return self.list.count
     }
     
+    // 테이블 뷰 행 구성
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // 주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
         
-        // 테이블 셀 객체를 직접 생성하는 대신 dequeueReusableCell로부터 가져옴
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
         cell.textLabel?.text = row.title
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("선택된 행은 \(indexPath.row) 번째 행입니다.")
     }
     
     
