249048bbaec9ed8f36f8aa58ba7ad2de37a5da31
junhyuk <dejavukk1@gmail.com>
2020-01-07 00:14:01 +0900

    썸네일 이미지 추가.

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -12,21 +12,22 @@ class ListViewContoller: UITableViewController {
     
     // 튜플 아이템을 가진 배열로 정의된 데이터 세트
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비.", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "대만 청춘 로맨스, 너까지 다섯 걸음.", "2015-05-07", 9.19)
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비.", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "대만 청춘 로맨스, 너까지 다섯 걸음.", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
 
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumbnail) in self.dataset {
             let mvo = MovieVO()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumbnail = thumbnail
             
             datalist.append(mvo)
         }
@@ -63,6 +64,7 @@ class ListViewContoller: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
         return cell
     }
--- MyMovieChart/MovieCell.swift
+++ MyMovieChart/MovieCell.swift
@@ -14,6 +14,7 @@ class MovieCell: UITableViewCell {
     @IBOutlet var opendate: UILabel!
     @IBOutlet var desc: UILabel!
     @IBOutlet var rating: UILabel!
+    @IBOutlet var thumbnail: UIImageView!
     
     
     
