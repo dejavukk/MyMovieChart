4033a0801d09687bc26b6880762b7fc2d9fd34cc
junhyuk <dejavukk1@gmail.com>
2020-01-28 23:56:17 +0900

    영화관 정보를 읽어오는 API호출.

--- MyMovieChart/Controllers/TheaterListController.swift
+++ MyMovieChart/Controllers/TheaterListController.swift
@@ -10,18 +10,50 @@ import UIKit
 
 class TheaterListController: UITableViewController {
     
+    // API를 통해 불러온 데이터를 저장할 배열 변수
+    var list = [NSDictionary]()
     
-
+    // 읽어올 데이터의 시작위치
+    var startPoint = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    func callTheaterAPI() {
+        
+        // 1. URL을 구성하기 위한 상수값 선언
+        let requestURL = "http://swiftapi.rubypaper.co.kr:2029/theater/list"
+        let sList = 100 // 데이터 갯수
+        let type = "json"   // 데이터 형식
+        
+        // 2. 파라미터값들을 모아 URL객체로 정의.
+        let urlObj = URL(string: "\(requestURL)?s_page=\(self.startPoint)&s_list=\(sList)&type=\(type)")
+        
+        do {
+            
+            // 3. NSString객체를 이용하여 API를 호출하고 그 결과값을 인코딩된 문자열로 받아온다.
+            let stringData = try NSString(contentsOf: urlObj!, encoding: 0x80_000_422)
+            
+            // 4. 문자열로 받은 데이터를 UTF-8로 인코딩한 처리한 Data로 변환한다.
+            let encdata = stringData.data(using: String.Encoding.utf8.rawValue)
+        }
+        
+        do {
+            
+            
+            
+        } catch {
+            
+            
+        }
+        
+        
+        
+    }
+
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
